4600affa7e41744f33fd1b3f88231b73673a6a97
eddie <eddietheengr@gmail.com>
2023-05-05 23:36:11 -0400

    Update main.py

--- stepper_tester/main.py
+++ stepper_tester/main.py
@@ -8,22 +8,22 @@ import concurrent.futures
 import csv_logger
 import sys
 import change_config
-import plotting
+#import plotting
 from dataclasses import dataclass, fields
 
 # str(input('Model Number: ') or "17HS19-2004S1")
 model_number = 'LDO_42STH48-2504AC'
-test_id = '5.5.2023b'
+test_id = '5.5.23c'
 step_angle = 1.8
 motor_resistance = 1.5
 iron_constant = 0.01
 
-speed_start = 10  # int(input('Start Speed: ') or 50)
+speed_start = 20  # int(input('Start Speed: ') or 50)
 speed_end = 3000  # int(input('Ending Speed: ') or 300)
-speed_step = 10  # int(input('Speed Step: ') or 50)
+speed_step = 20  # int(input('Speed Step: ') or 50)
 
-tmc_start = 1.8  # float(input('TMC Current Start: ') or 0.5)
-tmc_end = 1.8  # float(input('TMC Current End: ') or 1.0)
+tmc_start = 1.4  # float(input('TMC Current Start: ') or 0.5)
+tmc_end = 1.4  # float(input('TMC Current End: ') or 1.0)
 tmc_step = 0.8  # float(input('TMC Current Step: ') or 0.1)
 # tmc_array_5160_small = [0.09, 0.18, 0.26, 0.35, 0.44, 0.53, 0.61, 0.70, 0.79, 0.88, 0.96, 1.14, 1.23, 1.31, 1.40, 1.49, 1.58, 1.66, 1.84, 1.93, 2.01, 2.10, 2.19, 2.28, 2.36, 2.54, 2.63, 2.71, 2.80]
 tmc_array_5160 = [0.08, 0.16, 0.23, 0.31, 0.39, 0.47, 0.63, 0.70, 0.78, 0.86, 0.94, 1.02, 1.09, 1.17, 1.25,
@@ -32,9 +32,9 @@ tmc_array_5160 = [0.08, 0.16, 0.23, 0.31, 0.39, 0.47, 0.63, 0.70, 0.78, 0.86, 0.
 # microstep_array_complete = [1, 2, 4, 8, 16, 32, 64, 128]
 microstep_array = [16]
 
-voltage_start = 48
+voltage_start = 12
 voltage_end = 48
-voltage_step = 36
+voltage_step = 12
 
 reset_counter = 1
 
